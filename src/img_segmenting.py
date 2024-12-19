04b3d4cbfdbc02b12bf52da25a33f352282b050a
Your Name <you@example.com>
2024-12-19 12:41:03 -0500

    changes

--- src/img_segmenting.py
+++ src/img_segmenting.py
@@ -2,13 +2,13 @@ import cv2
 import argparse
 
 argParser = argparse.ArgumentParser()
-argParser.add_argument("-v", type=str, default='../data/test.mp4', help="directory of source video file")
+argParser.add_argument("-v", type=str, default='data/test.mp4', help="directory of source video file")
 args = argParser.parse_args()
 
 vid = cv2.VideoCapture(args.v)
 img_counter = 0
 read, img = vid.read()
 while read:
-    cv2.imwrite(f"../data/images_{(img_counter%3) + 1}/img_{img_counter}.jpg", img)
+    cv2.imwrite(f"data/images_{(img_counter%3) + 1}/img_{img_counter}.jpg", img)
     read, img = vid.read()
     img_counter += 1
