06bce2bde606c2e04f2b9804e966931cb49eee9c
Vid Grujic <vidgrujic@viii-mbp.home>
2025-01-09 20:45:19 -0500

    Tested servos, jeston triggering them to work

--- src/motors/servo_test_001.py
+++ src/motors/servo_test_001.py
@@ -1,11 +1,15 @@
-
+print("t1")
 from adafruit_servokit import ServoKit
-myKit=ServoKit(channels=16)
+kit=ServoKit(channels=16)
+print("t2")
 import time
+print("t3")
 while True:
     for i in range(0,180,1):
-        myKit.servo[0].angle=i
-        time.sleep(0.01)
+        print("t4")
+        kit.servo[0].angle=i
+        time.sleep(0.001)
     for i in range(180,0,-1):
-        myKit.servo[0].angle=i
-        time.sleep(0,01)
+        print("t5")
+        kit.servo[0].angle=i
+        time.sleep(0.001)
