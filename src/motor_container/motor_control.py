0504ba856b7d8b4450354fe78fa7886ada7332d9
Aaron Moise <tumsifu101@gmail.com>
2025-03-06 02:44:27 -0500

    new container branch

---
+++ src/motor_container/motor_control.py
@@ -0,0 +1,14 @@
+import zmq
+import msgpack
+
+context = zmq.Context()
+socket = context.socket(zmq.SUB) 
+socket.connect("ipc:///tmp/foosball")
+socket.setsockopt_string(zmq.SUBSCRIBE, "")
+socket.setsockopt(zmq.CONFLATE, 1)
+print("Motor Container is running... Waiting for data.")
+
+while True:
+    message = socket.recv()  # ✅ Blocking call, waits for data
+    data = msgpack.unpackb(message)
+    print(f"Received Ball Position: X={data['ball_x']}, Y={data['ball_y']}")
